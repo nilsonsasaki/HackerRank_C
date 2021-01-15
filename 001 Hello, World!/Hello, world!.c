32cc09d88e32ba794e2e95f289a3de2d304635a5
nilsonsasaki <70772836+nilsonsasaki@users.noreply.github.com>
2021-01-15 08:28:44 -0300

    Update Hello, world!.c

---
+++ 001 Hello, World!/Hello, world!.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    /*
+    -------------------------------English----------------------------------
+    Just added one line with the required text, escape sequence for new line
+    and printing a string
+    
+   ----------------------------Português(Brasil)----------------------------
+    Adicionei apenas uma linha contendo o texto exigido, a sequencia de escape 
+    para nova linha (\n) e imprimindo uma string
+    */
+    printf("Hello, World!\n%s",s);
+        
+    return 0;
+}
