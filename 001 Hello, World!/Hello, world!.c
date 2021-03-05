5d6eaacf0bd21a41f2e9650bb91d754c5a14cdd6
Nilson Takayuki Sasaki <nilsonsasaki@gmail.com>
2021-03-05 13:28:27 -0300

    scanf correction

--- 001 Hello, World!/Hello, world!.c
+++ 001 Hello, World!/Hello, world!.c
@@ -7,7 +7,7 @@ int main()
 {
 	
     char s[100];
-    scanf("%[^\n]%*c", &s);
+    scanf("%[^\n]%*c", s);
   	
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     /*
