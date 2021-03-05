069d80bb45fe76404594c7dcdf2990efef6f723e
Nilson Takayuki Sasaki <nilsonsasaki@gmail.com>
2021-03-05 13:37:45 -0300

    scanf correction

--- 002 Playing with Characters/Playing with Characters.c
+++ 002 Playing with Characters/Playing with Characters.c
@@ -8,8 +8,8 @@ int main()
     max length =100    
     */
     scanf("%c%*c", &ch); // *c is reading and eliminating '\n' so it won't be read in the next scanf
-    scanf("%[^\n]%*c", &s); // scanf is reading until before '\n', so i need to eliminate '\n' again
-    scanf("%[^\n]%*c", &sen);
+    scanf("%[^\n]%*c", s); // scanf is reading until before '\n', so i need to eliminate '\n' again
+    scanf("%[^\n]%*c", sen);
     printf("%c\n%s\n%s",ch,s,sen); //printing all variables
     getchar(); //wait a char after printf
     return 0;
