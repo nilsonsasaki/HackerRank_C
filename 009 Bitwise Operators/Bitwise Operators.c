78ec397d79fbefacbabe43da035be4701860a389
Nilson Takayuki Sasaki <nilsonsasaki@gmail.com>
2021-03-06 16:47:02 -0300

    added solution to Bitwise Operators

---
+++ 009 Bitwise Operators/Bitwise Operators.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+void calculate_the_maximum(int n, int k) {
+    int and = 0, or = 0, xor = 0;
+    
+  for(int i=1; i<=n;i++){
+      
+        for (int j=1;j<=k&&j<i;j++){
+            
+            if(((i&j)>and)&&((i&j)<k)){
+                
+                and=i&j;
+            }
+            
+            if(((i|j)>or)&&((i|j)<k)){
+                
+                or=i|j;
+            }
+            
+            if(((i^j)>xor)&&((i^j)<k)){
+                
+                xor = i^j;
+            }
+                
+        }
+    }
+    
+    printf("%d\n%d\n%d", and,or,xor);
+}
+
+int main() {
+
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+        
+    return 0;
+}
