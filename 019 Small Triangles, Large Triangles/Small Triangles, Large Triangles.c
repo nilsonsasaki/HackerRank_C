6427ef1e12a228969ff0acce044227c660a60b09
Nilson Takayuki Sasaki <nilsonsasaki@gmail.com>
2021-03-10 10:45:20 -0300

    testing another solution

--- 019 Small Triangles, Large Triangles/Small Triangles, Large Triangles.c
+++ 019 Small Triangles, Large Triangles/Small Triangles, Large Triangles.c
@@ -12,61 +12,63 @@ typedef struct triangle triangle;
 
 long int calculate_perimeter (int a, int b, int c){
 
-	long int perimeter = (long int) a + (long int) b + (long int) c;
-	return(perimeter);  
+    long int perimeter = (long int) a + (long int) b + (long int) c;
+    return(perimeter);  
 }
 
 long square_of_area (int a, int b, int c){
 
-	
-	long int p = (calculate_perimeter (a,b,c))/2;
-	long int sq_of_area = p*(p-a)*(p-b)*(p-c);
-	
-	return (sq_of_area);
-	
+    
+    long int p = (calculate_perimeter (a,b,c))/2;
+    long int sq_of_area = p*(p-a)*(p-b)*(p-c);
+    
+    return (sq_of_area);
+    
 }
 
 void get_all_areas (triangle* tr, int n, long int* areas){
 
-	for (int i=0; i<n; i++){
-		
-		*(areas+i) = square_of_area((triangle+i)->a,(triangle+i)->b,(triangle+i)->c);
-			
-	}
+    for (int i=0; i<n; i++){
+        
+        *(areas+i) = square_of_area((tr+i)->a,(tr+i)->b,(tr+i)->c);
+            
+    }
 }
 
-void swap_area (long int *a, long int *b){
+void put_before_first (int item_index,int first_index, long int *areas,  triangle * tr){
 
-	long int area_swap = *a;
-	*a = *b;
-	*b = area_swap;
+
+	long int area_swap = areas[item_index];
+	triangle tr_swap = tr[item_index];
 	
-}
+	for (int i=item_index; i>=0;i--){
 
-void swap_triangle (triangle* tr1, triangle* tr2){
-
-	triangle triangle_swap = *tr1;
-	*tr1 =*tr2;
-	*tr2 = triangle_swap;
+		areas[i] = areas[i-1];
+		tr[i] = tr[i-1];
+	}
+	areas[first_index] = area_swap;
+	tr[first_index] = tr_swap;
+	
 }
 
 
 void sort_by_area(triangle* tr, int n) {
 
-	long int *areas = malloc (n*sizeof(long int));
-	
-	triangle tringle_swap;
-	
-	get_all_areas (tr, n, areas);
-	
-	for(int i=0; i<n;i++){
-	
-		
-	}
-	/**
-	* Sort an array a of the length n
-	*/
+    long int *areas = malloc (n*sizeof(long int));
+    get_all_areas (tr, n, areas);
+    
+    for(int i=0; i<n-1;i++){
+    
+        for (int j=1; j<n; j++){
+        
+            if (areas[i]> areas[j]){
+            
+                put_before_first(j,i,areas,tr);
+            }
+        }
+    }
 }
+
 
 int main()
 {
