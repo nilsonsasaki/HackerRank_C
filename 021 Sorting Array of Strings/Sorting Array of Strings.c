1a84d080ad5bfd1b36459607e687999990b5b6bf
Nilson Takayuki Sasaki <nilsonsasaki@gmail.com>
2021-03-15 10:58:46 -0300

    correction on sort_by_number_of_distinct_characters

--- 021 Sorting Array of Strings/Sorting Array of Strings.c
+++ 021 Sorting Array of Strings/Sorting Array of Strings.c
@@ -15,15 +15,15 @@ int lexicographic_sort_reverse(const char* a, const char* b) {
 
 int sort_by_number_of_distinct_characters(const char* a, const char* b) {
 
-	*int a_ascii_list = calloc (256, sizeof(int));
-	*int b_ascii_list = calloc (256, sizeof(int));
+	int *a_ascii_list = calloc (256, sizeof(int));
+	int *b_ascii_list = calloc (256, sizeof(int));
 	int is_end_of_a = 0;
 	int is_end_of_b = 0;
 	int index = 0;
-	int a_distinct_characters, b_distinct_characters;
-	unsigned int ascii;
+	unsigned int a_distinct_characters =0, b_distinct_characters=0;
+	unsigned int ascii=0;
 	
-	while (!is_end_of_a && !is end of b){
+	while (!is_end_of_a && !is_end_of_b){
 	
 		if(*(a+index)=='\0'&& is_end_of_a==0)
 			is_end_of_a=1;
@@ -42,18 +42,22 @@ int sort_by_number_of_distinct_characters(const char* a, const char* b) {
 			ascii = (unsigned int)*(b+index);
 			b_ascii_list[ascii]++;
 		}
-		
-		for(int i=0;i<256;i++){
-			
-			a_distinct_characters = a_distinct_characters + a_ascii_list[i];
-			free(a_ascii_list);
-			b_distinct_characters = b_distinct_characters + b_ascii_list[i];
-			free(b_ascii_list);
-		}
-		
-
+					
 		index++;
 	}
+	
+	for(int i=0;i<256;i++){
+			
+		if(a_ascii_list[i]>0)
+			a_distinct_characters++;
+			
+		if(b_ascii_list[i]>0)
+			b_distinct_characters++;
+			
+	}
+	
+	free(a_ascii_list);
+	free(b_ascii_list);
 	
 	if(a_distinct_characters == b_distinct_characters)
 		return(0);
@@ -61,7 +65,7 @@ int sort_by_number_of_distinct_characters(const char* a, const char* b) {
 	if(a_distinct_characters >b_distinct_characters)
 		return(1);
 	
-	if(a_distinct_characters >b_distinct_characters)
+	if(a_distinct_characters <b_distinct_characters)
 		return(-1);
 }
 	
