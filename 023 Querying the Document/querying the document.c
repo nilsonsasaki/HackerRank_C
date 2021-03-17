f203b60e01200ac08e9b90f5834116e7d5844c8b
Nilson Takayuki Sasaki <nilsonsasaki@gmail.com>
2021-03-17 13:09:58 -0300

    corrected all functions indexes

--- 023 Querying the Document/querying the document.c
+++ 023 Querying the Document/querying the document.c
@@ -7,21 +7,22 @@
 
 char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
 
-	return(document[n][m][k]);
+	return(document[n-1][m-1][k-1]);
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
 
-	return(document[m][k]);
+	return(document[m-1][k-1]);
 }
 
 char*** kth_paragraph(char**** document, int k) {
 
-	return(document[k])
+	return(document[k-1]);
 }
 
 char**** get_document(char* text) {
 
+	
 }
 
 
